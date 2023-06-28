adc5a45c0c66a0b3fab52c8956e745f5bfc37422
SCDN01 <asherjanseluy@gmail.com>
2023-06-28 18:57:08 +0800

    Add scene loader

---
+++ Assets/Scripts/LivesCounterDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class LivesCounterDisplay : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    public LivesCounter livesCounter; 
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = "LIVES: " + livesCounter.health;
+    }
+
+   
+    void Update()
+    {
+        text.text = "LIVES: " + livesCounter.health; 
+    }
+}
